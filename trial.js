281049c6404009f0a87e8aa4fc6ef7d877dbc984
chikafx <kingsobi70@yahoo.com>
2024-07-26 08:14:16 +0100

    feat: fixed bug

--- trial.js
+++ trial.js
@@ -5,22 +5,33 @@ const IdSec = document.querySelector('#third')
 const lists = document.querySelectorAll('ol li')
 const eight = document.querySelector('li.june25:nth-child(8)')
 const nine = eight.nextElementSibling
+const june22 = document.getElementsByClassName('june22')
+lists[4].style.backgroundColor = 'blue'
+const june21 =document.getElementsByTagName('june21')
+lists[3].textContent = 'who goes'
+const june24 = document.getElementsByTagName('june24')
+lists[1].textContent = 'i will show mercy on whom i want to show mercy'
+lists[6].textContent = 'form zero knowledge of coding to 50% of coding knowledge'
+const june25 = document.getElementsByClassName('june25')
+lists[7].style.backgroundColor = 'pink'
+const ten = nine.nextElementSibling
+ten.textContent = 'life is not easy'
+ten.style.textTransform = 'capitalize'
+ten.style.backgroundColor = 'orange'
 
 
-
-
-
+lists[6].style.backgroundColor = 'indigo'
 nine.textContent = 'Jesus is Lord'
 nine.style.backgroundColor = 'green'
 nine.style.textTransform = 'capitalize'
-nine.style.paddingBlock = '2em'
+nine.style.paddingBlock = '0em'
 IdSec.textContent = 'God help us all'
 IdSec.style.backgroundColor = 'purple'
-IdSec.style.paddingBlock = '2em'
+IdSec.style.paddingBlock = '0em'
 IdSec.style.textTransform = 'capitalize'
 IdSec.style.color = 'red'
 lists[0].textContent = 'Java script seems interesting'
 lists[0].style.textTransform = 'capitalize'
 lists[0].style.backgroundColor = 'red'
-lists[0].style.paddingBlock = '2em'
+lists[0].style.paddingBlock = '0em'
 lists.style.backgroundColor = 'red'
